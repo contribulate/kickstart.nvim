769560f148a9777d06a5b115dea652739dd978d1
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-09-01 23:25:12 +0200

    :sparkles: feat(gui): Clean up GUIs.

---
+++ lua/custom/plugins/gui.lua
@@ -0,0 +1,19 @@
+-- gui
+return {
+  'nvim-lua/plenary.nvim',
+  event = 'UIEnter',
+  config = function()
+    -- clean up the GUI
+    if vim.fn.has 'gui_running' == 1 then
+      vim.opt.guioptions:remove 'm' -- remove menu bar
+      vim.opt.guioptions:remove 'T' -- remove toolbar
+      vim.opt.guioptions:remove 'r' -- remove right-hand scroll bar
+      vim.opt.guioptions:remove 'L' -- remove left-hand scroll bar
+
+      vim.opt.lines = 60
+      vim.opt.columns = 108
+      vim.opt.linespace = 0
+      vim.opt.guifont = 'LiterationMono Nerd Font Mono:h12'
+    end
+  end,
+}
