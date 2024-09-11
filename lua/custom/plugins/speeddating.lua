879cde475c9ce750cef566b31a971dbc9505ea99
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-09-11 16:51:10 +0200

    :sparkles: feat: Add `speeddating` plugin.

---
+++ lua/custom/plugins/speeddating.lua
@@ -0,0 +1,10 @@
+-- speeddating
+
+return {
+  -- use CTRL-A/CTRL-X to increment dates, times, and more
+  'tpope/vim-speeddating',
+  dependencies = {
+    -- enable repeating speeddating mappings with "."
+    'tpope/vim-repeat',
+  },
+}
